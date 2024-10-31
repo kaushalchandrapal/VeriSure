cee0729d8f23d7e4c2903851f89915a119004859
Nidhi Pansuriya <pnidhic36@gmail.com>
2024-10-31 11:45:15 -0400

    backend setup | 1

--- apps/verisure-server/vite.config.ts
+++ apps/verisure-server/vite.config.ts
@@ -1,16 +1,27 @@
-// apps/my-app/vite.config.js
 import { defineConfig } from 'vite';
 import { VitePluginNode } from 'vite-plugin-node';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
     VitePluginNode({
       adapter: 'express',
-      appPath: './src/main.ts',
+      appPath: path.resolve(__dirname, './src/main.ts'),
       exportName: 'default',
     }),
   ],
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: path.resolve(__dirname, 'src/main.ts'),
+    },
+  },
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
